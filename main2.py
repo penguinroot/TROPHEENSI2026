10e373a4cb01fa2f29b2097883874b368ceb5bee
n2geek <noam.duboiscossard@gmail.com>
2026-01-16 10:10:04 +0100

    Update

--- main2.py
+++ main2.py
@@ -2,154 +2,111 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
-from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import (
-    Point3, Vec4, AmbientLight, DirectionalLight,
-    Spotlight, PerspectiveLens, Fog, WindowProperties
-)
-from direct.filter.CommonFilters import CommonFilters
-
+from panda3d.core import Point3, Vec3
+from direct.gui.DirectGui import DirectButton
+from panda3d.core import WindowProperties
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.disableMouse()
-        self.render.setAntialias(True)
 
-        # --------------------
-        # Fenêtre
-        # --------------------
-        props = WindowProperties()
-        props.setTitle("Panda3D - Cinematic RTX-like Demo")
-        self.win.requestProperties(props)
-
-        # --------------------
-        # Décor
-        # --------------------
+        # --- ENVIRONNEMENT ---
         self.scene = self.loader.loadModel("models/environment")
         self.scene.reparentTo(self.render)
         self.scene.setScale(0.25)
         self.scene.setPos(-8, 42, 0)
-        self.scene.setShaderAuto()
 
-        # --------------------
-        # Panda
-        # --------------------
-        self.pandaActor = Actor(
-            "models/panda-model",
-            {"walk": "models/panda-walk4"}
-        )
+        # --- PANDA ACTEUR ---
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005)
-        self.pandaActor.setPos(0, 10, 0)
         self.pandaActor.reparentTo(self.render)
-        self.pandaActor.setShaderAuto()
         self.pandaActor.loop("walk")
 
-        # --------------------
-        # Lumières
-        # --------------------
-        # Lumière ambiante faible
-        ambient = AmbientLight("ambient")
-        ambient.setColor(Vec4(0.08, 0.08, 0.1, 1))
-        self.render.setLight(self.render.attachNewNode(ambient))
+        # --- CAMERA FIXE ---
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        self.fixedCamera = True
 
-        # Soleil principal avec ombres HD
-        self.sun = DirectionalLight("sun")
-        self.sun.setColor(Vec4(1.4, 1.35, 1.2, 1))
-        self.sun.setShadowCaster(True, 4096, 4096)
-        self.sunNP = self.render.attachNewNode(self.sun)
-        self.sunNP.setHpr(-45, -60, 0)
-        self.render.setLight(self.sunNP)
+        # --- CAMERA POV DU PANDA ---
+        self.povPos = Point3(0, 0.5, 1000)
+        self.povHpr = Point3(180, 0, 0)
 
-        # Projecteur cinéma sur le panda
-        lens = PerspectiveLens()
-        lens.setFov(40)
-        self.spot = Spotlight("spot")
-        self.spot.setLens(lens)
-        self.spot.setColor(Vec4(1.8, 1.6, 1.4, 1))
-        self.spot.setShadowCaster(True, 2048, 2048)
-        self.spotNP = self.render.attachNewNode(self.spot)
-        self.spotNP.setPos(0, -5, 8)
-        self.spotNP.lookAt(self.pandaActor)
-        self.render.setLight(self.spotNP)
+        # --- BOUTON GUI POUR BASCULER LES VUES ---
+        self.toggleButton = DirectButton(text="Changer Vue",
+                                         scale=0.07,
+                                         pos=(0.8, 0, 0.9),
+                                         command=self.toggleCameraView)
 
-        # Activation globale des shaders (obligatoire pour ombres)
-        self.render.setShaderAuto()
+        # --- CONTRÔLE DU PANDA ---
+        self.accept("z", self.setKey, ["backward", True])
+        self.accept("z-up", self.setKey, ["backward", False])
+        self.accept("s", self.setKey, ["forward", True])
+        self.accept("s-up", self.setKey, ["forward", False])
+        self.accept("q", self.setKey, ["turnLeft", True])
+        self.accept("q-up", self.setKey, ["turnLeft", False])
+        self.accept("d", self.setKey, ["turnRight", True])
+        self.accept("d-up", self.setKey, ["turnRight", False])
 
-        # --------------------
-        # Brouillard atmosphérique
-        # --------------------
-        fog = Fog("fog")
-        fog.setColor(0.2, 0.25, 0.3)
-        fog.setExpDensity(0.015)
-        self.render.setFog(fog)
+        self.keyMap = {
+            "forward": False,
+            "backward": False,
+            "turnLeft": False,
+            "turnRight": False
+        }
 
-        # --------------------
-        # Effet Bloom (compatible toutes versions)
-        # --------------------
-        self.filters = CommonFilters(self.win, self.cam)
-        self.filters.setBloom(
-            blend=(0.3, 0.3, 0.3, 0.0),
-            desat=0.2,
-            intensity=3.0,
-            size="medium"
-        )
+        self.speed = 1000        # vitesse de déplacement (unités/seconde)
+        self.turnSpeed = 60   # vitesse de rotation (degrés/seconde)
 
-        # --------------------
-        # Animation du panda
-        # --------------------
-        move1 = self.pandaActor.posInterval(
-            6, Point3(4, -10, 0), startPos=Point3(-4, 10, 0)
-        )
-        rot1 = self.pandaActor.hprInterval(
-            2, Point3(180, 0, 0)
-        )
-        move2 = self.pandaActor.posInterval(
-            6, Point3(-4, 10, 0)
-        )
-        rot2 = self.pandaActor.hprInterval(
-            2, Point3(0, 0, 0)
-        )
+        self.taskMgr.add(self.updatePanda, "UpdatePandaTask")
 
-        self.pandaSeq = Sequence(
-            move1, rot1, move2, rot2,
-            name="pandaSeq"
-        )
-        self.pandaSeq.loop()
+    # --- GESTION DES TOUCHES ---
+    def setKey(self, key, value):
+        self.keyMap[key] = value
 
-        # --------------------
-        # Caméra cinématique
-        # --------------------
-        self.taskMgr.add(self.cinematicCameraTask, "CinematicCamera")
+    # --- MOUVEMENT DU PANDA ---
+    def updatePanda(self, task):
+        dt = globalClock.getDt()  # temps depuis la dernière frame
 
-    def cinematicCameraTask(self, task):
-        t = task.time
+        # Rotation
+        if self.keyMap["turnLeft"]:
+            self.pandaActor.setH(self.pandaActor.getH() + self.turnSpeed * dt)
+        if self.keyMap["turnRight"]:
+            self.pandaActor.setH(self.pandaActor.getH() - self.turnSpeed * dt)
 
-        # Trajectoire dynamique type drone
-        radius = 30 + 5 * sin(t * 0.7)
-        angle = t * 0.6
+        # Déplacement
+        direction = Vec3(0, 0, 0)
+        if self.keyMap["forward"]:
+            direction.y += 1
+        if self.keyMap["backward"]:
+            direction.y -= 1
 
-        x = radius * sin(angle)
-        y = -radius * cos(angle)
-        z = 6 + 2 * sin(t * 1.5)
-
-        self.camera.setPos(x, y, z)
-        self.camera.lookAt(self.pandaActor)
-
-        # Léger roulis pour effet cinéma
-        roll = 5 * sin(t)
-        self.camera.setR(roll)
-
-        # Le projecteur suit le panda
-        self.spotNP.setPos(
-            self.pandaActor.getX(),
-            self.pandaActor.getY() - 6,
-            10
-        )
-        self.spotNP.lookAt(self.pandaActor)
+        if direction.length() > 0:
+            direction.normalize()
+            # Déplacer dans la direction locale du panda
+            self.pandaActor.setPos(self.pandaActor, direction * self.speed * dt)
 
         return Task.cont
 
+    # --- CAMERA FIXE AUTOUR DE LA SCÈNE ---
+    def spinCameraTask(self, task):
+        if self.fixedCamera:
+            angleDegrees = task.time * 6.0
+            angleRadians = angleDegrees * (pi / 180.0)
+            self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+            self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+    # --- BASCULER ENTRE LES VUES ---
+    def toggleCameraView(self):
+        if self.fixedCamera:
+            self.fixedCamera = False
+            self.camera.reparentTo(self.pandaActor)
+            self.camera.setPos(self.povPos)
+            self.camera.setHpr(self.povHpr)
+        else:
+            self.fixedCamera = True
+            self.camera.reparentTo(self.render)
 
 app = MyApp()
 app.run()
