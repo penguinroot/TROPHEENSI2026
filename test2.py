11f6828e9ab1e4edea01da4acf166756e3e535af
PMB <pm.bard@free.fr>
2026-02-04 21:39:08 +0100

    Implement Lapin behavior for food consumption and enhance Nourriture class

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import pygame
-import random
-import math
+from random import *
+from math import *
 
 # --- Initialisation ---
 pygame.init()
@@ -10,7 +10,17 @@ screen = pygame.display.set_mode((LARGEUR, HAUTEUR))
 pygame.display.set_caption("Écosystème vivant")
 VERT = (34, 139, 34)
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 600
+class Nouritture():
+    def __init__(self,x,y,color,):
+        self.x=x
+        self.y=y
+        self.color=color
+    def dessiner(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, 10, 10))
+class plante(Nouritture):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
 
 class Animal() :
     def __init__(self,speed,x,y,color):
@@ -20,15 +30,46 @@ class Animal() :
         self.color=color
     def dessiner(self):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), 5)
-        
+
+
+class Lapin(Animal):
+    def __init__(self, speed, x, y, color):
+        super().__init__(speed, x, y, color)
+    def aller_manger(self,lst_nouritture):
+        dist_min=max(LARGEUR,HAUTEUR)
+        nourr_plus_proche=0
+        for i in lst_nouritture :
+            dist_act=sqrt((i.x-self.x)**2+(i.y-self.y)**2)
+            if dist_act < dist_min :
+                dist_min=dist_act
+                nourr_plus_proche = i
+        if nourr_plus_proche == 0 : 
+            return None
+        if nourr_plus_proche.x > self.x :
+            self.x+=1
+        elif nourr_plus_proche.x < self.x :
+            self.x-=1
+        if nourr_plus_proche.y > self.y :
+            self.y+=1
+        elif nourr_plus_proche.y < self.y :
+            self.y-=1
+        if nourr_plus_proche.y == self.y and nourr_plus_proche.x == self.x :
+            lst_nouritture.remove(nourr_plus_proche)
+            return lst_nouritture
 running = True
+lst_lapin=[Lapin(2, 10, 10, (255, 255, 255)) for _ in range(1)]
+lst_nouritture = [Nouritture(100+randint(1,300), 100+randint(1,300), (0, 125, 0)) for _ in range(100)]
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill(VERT)
-
+    for i in lst_lapin :
+        i.aller_manger(lst_nouritture)
+        i.dessiner()
+    for j in lst_nouritture :
+        j.dessiner()
     pygame.display.flip()
     clock.tick(FPS)
 
